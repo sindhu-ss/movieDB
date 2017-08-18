db77cadf789bee7dbdf5d22f420b04a30cc0a97b
Sindhu <sindhuss@gmail.com>
2017-08-18 12:22:52 +1200

    Add project files.

---
+++ Lumen-MovieDB/Models/Movies.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lumen_MovieDB.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public int ReleaseYear { get; set; }
+        public string MovieTitle { get; set; }
+        public string Description { get; set; }
+        public string MovieGenre { get; set; }
+        public string featuredImg { get; set; }
+
+        public string Username { get; set; }
+        public string Comment { get; set; }
+    }
+}
